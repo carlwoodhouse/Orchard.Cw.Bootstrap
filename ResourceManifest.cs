e3731f835333f64e593d0306c50b8d195336deeb
Carl Woodhouse <carl.woodhouse@gmail.com>
2015-09-02 16:16:15 +0100

    Added

---
+++ ResourceManifest.cs
@@ -0,0 +1,16 @@
+﻿using Orchard.UI.Resources;
+
+namespace Orchard.Cw.Bootstrap {
+    public class ResourceManifest : IResourceManifestProvider {
+        public void BuildManifests(ResourceManifestBuilder builder) {
+            var manifest = builder.Add();
+
+            manifest.DefineScript("Bootstrap.Base.Script").SetUrl("assets/js/bootstrap.min.js", "assets/js/bootstrap.js")
+                .SetVersion("3.3.5")
+                .SetDependencies("jQuery");
+      
+
+            manifest.DefineStyle("Bootstrap.Base.Style").SetUrl("assets/css/bootstrap.min.css", "assets/css/bootstrap.css");
+        }
+    }
+}
